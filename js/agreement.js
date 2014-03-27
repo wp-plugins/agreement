9665fb190185f8a86eaf57f5db6e5f701a166e78
pranavpathakjaora <pranavpathakjaora@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-27 16:02:16 +0000

    Now it works for multiple item in single page.
    git-svn-id: https://plugins.svn.wordpress.org/agreement/trunk@883014 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/agreement.js
+++ js/agreement.js
@@ -1,24 +1,29 @@
 // jQuery for disable link and redirect to new window if click on agree.
-	jQuery(document).ready(function() {
-		var href = jQuery('#agreement').attr('href');
-		jQuery('#agreement').prop("href", "#");	
-		jQuery('#agreement').click(function(e) {
-		e.preventDefault();
-		jQuery('.licence-agreementbase').show().css("top", "0px");	
-		// calclution for get windows hight and width for popup.
-		jQuery('.licence-agreement').show().css("position","absolute");
-		jQuery('.licence-agreement').css("top", Math.max(0, ((jQuery(window).height() - jQuery('.licence-agreement').outerHeight()) / 2) + jQuery(window).scrollTop()) + "px");
-		jQuery('.licence-agreement').css("left", Math.max(0, ((jQuery(window).width() - jQuery('.licence-agreement').outerWidth()) / 2) +  jQuery(window).scrollLeft()) + "px");
-	});
-	//Agree button redirect to new window with url.
-	jQuery('#agree').click(function(){
-		jQuery('.licence-agreementbase').fadeOut('slow');
-		jQuery('.licence-agreement').hide();
-		window.open(href,'_blank');
-	});	
-	//disagree button clear everthing.
-	jQuery('#dis-agree').click(function(){
-		jQuery('.licence-agreementbase').fadeOut('slow');
-		jQuery('.licence-agreement').hide();
-	});		
+jQuery(document).ready(function() {
+    // collecting all agreements links into an array 
+    var agree_elements = jQuery('a[id="agreement"]').clone().toArray();
+    var href;
+
+    jQuery('a[id="agreement"]').map(function(i,v){
+        return jQuery(this).prop("href", "#").click(function(e){
+                href = agree_elements[i];
+                e.preventDefault();
+                //calculation for get windows hight and width for popup.
+                jQuery('.licence-agreement').show().css("position","absolute");
+                jQuery('.licence-agreement').css("top", Math.max(0, ((jQuery(window).height() - jQuery('.licence-agreement').outerHeight()) / 2) + jQuery(window).scrollTop()) + "px");
+                jQuery('.licence-agreement').css("left", Math.max(0, ((jQuery(window).width() - jQuery('.licence-agreement').outerWidth()) / 2) +  jQuery(window).scrollLeft()) + "px");
+            })
+    });
+
+    //Agree button redirect to new window with url.
+    jQuery('#agree').click(function(){
+        jQuery('.licence-agreementbase').fadeOut('slow');
+        jQuery('.licence-agreement').hide();
+        window.open(href,'_blank');
+    });    
+    //disagree button clear everything.
+    jQuery('#dis-agree').click(function(){
+        jQuery('.licence-agreementbase').fadeOut('slow');
+        jQuery('.licence-agreement').hide();
+    });
 });
